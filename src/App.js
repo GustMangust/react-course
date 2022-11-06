a32a54717a8f5b4f9d319a61fa17dea8e8311b78
GustMangust <gust.mangust@gmail.com>
2022-11-07 00:17:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ function App() {
 
   return (
     <div className="App">
-      
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JS</strong>
+          <div>
+            JS-language
+          </div>
+        </div>
+        <div className="post__btns ">
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
